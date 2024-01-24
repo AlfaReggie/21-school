71d345696f95c7bac29b6cb7c07e94407821467e
Marth Karyn <marthkar@mi-f5.msk.21-school.ru>
2024-01-24 19:57:39 +0300

    update 20:00 24.01.24

--- 21grep_inWork/test_grep.c
+++ 21grep_inWork/test_grep.c
@@ -1,66 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
+#include <unistd.h>
 #include <getopt.h>
-#include <regex.h>
 
-#define FLAGS_COUNT 10
 #define SIZE 4096
 
-typedef struct s_flags {
-
-    char* patterns_files[SIZE];
-    char* files[SIZE];
-    char* pattern[SIZE];
+typedef struct s_flags
+{
     
-    int e;  // Шаблон |
-    int i;  // Игнорирует различия регистра.  |
-    int v;  // Инвертирует смысл поиска соответствий. |
-    int c;  // Выводит только количество совпадающих строк. |
-    int l;  // Выводит только совпадающие файлы.  |
-    int n;  // Предваряет каждую строку вывода номером строки из файла ввода. |
-    int h;  // Выводит совпадающие строки, не предваряя их именами файлов. |
-    int s;  // Подавляет сообщения об ошибках о несуществующих |
-    int f;  // Получает регулярные выражения из файла. |
-    int o;  // Печатает только совпадающие (непустые) части совпавшей строки. |
+    char flag;
+    char template[SIZE];
 
 } t_flags;
 
-void* safety_malloc(size_t size) {
-    void* pointer = malloc(size);
-    if (!pointer)
-        exit(1);
-    return pointer;
-}
+// void* safety_malloc(size_t size) {
+//     void* pointer = malloc(size);
+//     if (!pointer)
+//         exit(1);
+//     return pointer;
+// }
 
-void* safety_realloc(void* ptr, size_t newsize) {
-    ptr = realloc(ptr, newsize);
-        if (!ptr)
-            exit(1);
-    return ptr;
-}
+// void* safety_realloc(void* ptr, size_t newsize) {
+//     ptr = realloc(ptr, newsize);
+//         if (!ptr)
+//             exit(1);
+//     return ptr;
+// }
+
+// void error(char *mesg, int errnum)
+// {
+//   if (errnum)
+//     fprintf(stderr, "s21_grep: %s: %s\n", mesg, strerror(errnum));
+//   else
+//     fprintf(stderr, "s21_grep: %s\n", mesg);
+// }
+
+// void fatal(char *mesg, int errnum)
+// {
+//   error(mesg, errnum);
+//   exit(2);
+// }
+
+// void *extended_realloc(void *ptr, size_t size)
+// {
+//   void *result;
+//   if (ptr)
+//     result = realloc(ptr, size);
+//   else
+//     result = malloc(size);
+//   if (size && !result)
+//     fatal("memory exhausted", 0);
+//   return result;
+// }
+
+// void* safety_malloc(size_t size)
+// {
+//   void* pointer = malloc(size);
+//   if (!pointer)
+//     exit(1);
+//   return pointer;
+// }
+
+// void* safety_realloc(void* ptr, size_t newsize)
+// {
+//   ptr = realloc(ptr, newsize);
+//   if (!ptr)
+//     exit(1);
+//   return ptr;
+// }
+
 
 void execute(int argc, char **argv)
 {
     int opt = 0;
     int num_flags = 0;
-    t_flags* s_flags = NULL;
+    t_flags* flags = NULL;
 
-    while ((opt = getopt(argc, argv, "e:ivclnhsf:o")) != -1)
+    while ((opt = getopt(argc, argv, "e:ivclnhsf:o")) != EOF)
     {
         if (opt == '?')
             exit(2);
-        if (!s_flags)
-            arrFlags = (t_flags*)safety_malloc(sizeof(t_flags));
-        else
-            arrFlags = (t_flags*)safety_realloc(s_flags, sizeof(t_flags)*(num_flags+1));
-
         printf("Start\n");
         printf("opt: %c\n", (char)opt);
         printf("optarg: %s\n", optarg);
-        t_flags. = opt;
-        _sflags[num_flags].template = optarg ? strdup(optarg) : optarg;
+        flags[num_flags].flag = opt;
+        flags->template[num_flags] = optarg;
         printf("flag[%d].flag: %c\n", num_flags, flags[num_flags].flag);
         printf("flag[%d].template: %s\n", num_flags, flags[num_flags].template);
         printf("num_flags: %d\n\n", num_flags);
@@ -68,7 +93,9 @@ void execute(int argc, char **argv)
     }
 }
 
-int main(int argc, char** argv) {
+
+int main(int argc, char** argv)
+{
     execute(argc, argv);
     return 0;
 }
